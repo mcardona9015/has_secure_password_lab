1837a45214ded44d0c88e1fdf6ba3fda317ac45a
mcardona9015 <mcardona9015@gmail.com>
2021-01-20 22:47:42 -0500

    commit tree 4bfaff48cb06bc89597310a6cd6874048c1a341f

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/welcome', to: 'users#welcome', as: 'welcome'
+
+  get '/users/login', to: 'sessions#new', as: 'login'
+  post '/users/login', to: 'sessions#create'
+  delete '/users/logout', to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
